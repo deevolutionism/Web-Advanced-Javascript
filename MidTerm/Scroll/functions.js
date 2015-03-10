16c02156e0c55fda3b55a0aba5d86afe450be8b1
deevolutionism <deevolutionism@gmail.com>
2015-03-10 18:52:26 -0400

    final touches

--- MidTerm/Scroll/functions.js
+++ MidTerm/Scroll/functions.js
@@ -3,6 +3,9 @@ var sliderVal = 0;
 var w = window;
 console.log(w.pageYOffset);
 
+var background;
+var backgroundColor;
+
 //http://www.javascriptkit.com/dhtmltutors/parallaxscrolling/
 window.requestAnimationFrame = window.requestAnimationFrame
  || window.mozRequestAnimationFrame
@@ -13,6 +16,7 @@ window.requestAnimationFrame = window.requestAnimationFrame
 
  var sidebar = document.getElementById('description');
 
+//capture image elements. Probably a more efficient way to do this... for loop + array??
  var image5 = document.getElementById('image5');
  var image4 = document.getElementById('image4');
  var image3 = document.getElementById('image3');
@@ -24,18 +28,26 @@ window.requestAnimationFrame = window.requestAnimationFrame
  function scale() {
   var scrolltop = window.pageYOffset;
   console.log(window.pageYOffset);
+
+  //a lot of hard coded values... hope to make this more of a system in the future.
+  //Change each image's position based on scroll distance from top.
   image5.style.transform = "translateZ(" + (window.pageYOffset - 1500) + "px)";
   image4.style.transform = "translateZ(" + (window.pageYOffset - 2000) + "px)";
   image3.style.transform = "translateZ(" + (window.pageYOffset - 2500) + "px)";
   image2.style.transform = "translateZ(" + (window.pageYOffset - 3000) + "px)";
   image1.style.transform = "translateZ(" + (window.pageYOffset - 3500) + "px)";
   
+  //set the initial starting position in the Z plane for each photo. About 500px between each.
   var position5 = window.pageYOffset - 1500;
   var position4 = window.pageYOffset - 2000;
   var position3 = window.pageYOffset - 2500;
   var position2 = window.pageYOffset - 3000;
   var position1 = window.pageYOffset - 3500;
+
+  //change background color as the user "increases in depth" (scrolls)
+  document.body.style.backgroundColor = "rgb(" + (255 - backgroundColor) + "," + (255 - backgroundColor) + "," + (255 - backgroundColor) + ")";
   
+  //change the opacity of the image after a certain threshold in order to reveal the next image in sequence.
   if (position5 > 0 && position5 < 100) {
     image5.style.opacity = .5;
     } else if (position5 > 100){
@@ -76,6 +88,7 @@ window.requestAnimationFrame = window.requestAnimationFrame
       image1.style.opacity = 1;
     }
 
+  //start screen looks similar to a file cabinet - provides overview look
   if (window.pageYOffset == 0) {
     image5.style.transform = "translateY(400px) translateZ(-1500px)";
     image4.style.transform = "translateY(200px) translateZ(-2000px)";
@@ -87,11 +100,11 @@ window.requestAnimationFrame = window.requestAnimationFrame
     image3.style.opacity = .8;
     image2.style.opacity = .8;
     image1.style.opacity = .8;
-    sidebar.innerHTML = "<h3>Gentry Demchak</h3>";
+    sidebar.innerHTML = "<h3>Gentry Demchak</h3><p>Design and Technology</p><p>Contact: deevolutionism@gmail.com</P><a href=https://twitter.com/?lang=en><p><img src=images/twitter.png</p></a>";
   } 
 
 
-
+  //
   if (position5 > -500 && position5 < 0){
     sidebar.innerHTML = "<h3>Sven Head</h3><h5>2015</h5><p>An effort to increase recycling on D12. We scanned, 3D printed, and animated Sven's head. We also recorded 15 quotes. He would react whenever someone deposited trash. Made in partnership with Shubhank Sahey, Yumeng Wang, and Yi Tang.</p>";
   } else if (position4 > -500 && position4 < 0) {
@@ -110,14 +123,30 @@ window.requestAnimationFrame = window.requestAnimationFrame
   // }
 
 
+
  };
+
+ //http://stackoverflow.com/questions/5649803/remap-or-map-function-in-javascript
+ //similar to processing's map function.
+ function map_range(value, low1, high1, low2, high2) {
+    return low2 + (high2 - low2) * (value - low1) / (high1 - low1);
+
+}
 
  // window.addEventListener('scroll', function(){
  //  scale;
  // })
 
+  //call the scroll function whenever the page is scrolled
  window.addEventListener('scroll', function(){
    // requestAnimationFrame(scale)
+
+   //store map_range function as a variable
+   background = map_range(window.pageYOffset,0,6384,0,255);
+   //convert to int because color will not accept floats
+   backgroundColor = background | 0; //convert float to int so color can be changed
+
+
    scale();
  }, false);
 
